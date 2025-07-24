adc7c734f417f7ccb2017fcc1b424d7f9da02503
Rithik Jakhmola <jakhmolarithik8@gmail.com>
2025-07-24 16:24:01 +0530

    Color Changing on click has been added

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function playGame (userChoice){
     document.getElementById("result").textContent = result;
     document.getElementById("score").textContent=`Your score:${userScore}| Computer score: ${computerScore}`;
 }
+function colorChange(e){
+    document.querySelectorAll("#rock, #paper,#scissors").forEach(btn=>{
+        btn.style.background = "";
+    })
+    e.target.style['background-color'] = "pink";
+}
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", function(e){
+    playGame("rock");
+    colorChange(e);
+} );
+
+const paper = document.querySelector("#paper"); 
+paper.addEventListener("click",function (e){
+    playGame("paper");
+    colorChange(e);
+});
+const scissors = document.querySelector("#scissors"); 
+scissors.addEventListener("click",function (e){
+    playGame("scissors");
+    colorChange(e);
+});
